5b7ddd57be0fc614e84d5bc281a15040a4f472ae
MarcinZB <marcin.zbroszczyk@outlook.com>
2023-01-13 20:12:12 +0100

    new

---
+++ Learning/lists.py
@@ -0,0 +1,29 @@
+def the_list(a,b,c):
+    the_list = [a,b,c]
+    counter = 0
+    for ix in range(len(the_list)):
+        print(the_list[ix], end=' ')
+        the_list[ix] = counter
+        counter +=1
+    for ix in range(len(the_list)):
+        print(the_list[ix], end=" ")
+   
+def list_try(a,b,c):
+    empty_list = []
+    three_elements = [a,b,c]
+    print(three_elements[-1])
+    print(empty_list)
+
+angle = 1
+"""for i in range(5):
+    if i % 2 ==1:
+        angle+=1
+else:
+    angle-=1
+"""
+
+the_list = [0,1,2,3]
+
+print(the_list[0:len(the_list)-1])
+print(the_list[:])
+print(the_list[-3:-1])
