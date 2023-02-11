33fd1254c14dc64bdb55729c7e97d59d8ab75345
MarcinZB <marcin.zbroszczyk@outlook.com>
2023-02-11 20:41:18 +0100

    New_structure

--- Learning_Projects/Math_painter/main.py
+++ Learning_Projects/Math_painter/main.py
@@ -10,15 +10,65 @@
 #     określa kolor tła. Sam decyduje kiedy kończy            #
 #     rysowanie figur.                                        #   
 ###############################################################
-from canvas import Canvas
+from PIL import Image
+import numpy as np
+
+class Canvas:
+
+    def __init__(self, width, height, color):
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def create_canvas(self):
+        self.canvas = np.zeros((self.height,self.width,3), dtype=np.uint8)
+        return self.canvas
+
+    def select_color(self):
+        
+        if self.color == "black":
+            self.canvas[:] = [0,0,0]
+        elif self.color == "white":
+            self.canvas[:] = [255,255,255]
+        else:
+            print("There are only two options black and white for now")
+            print("The color has been selected by program as a white")
+            self.canvas[:] = [255,255,255]
+        return self.canvas
+
+    def generate_img(self, final_art):
+        final_canvas = Image.fromarray(final_art, "RGB")
+        final_canvas.save("Canvas.png")
+
+
+
+class Square:
+
+    def __init__(self, x, y,color, edge):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.edge = edge
     
-canvas_height = int(input("Give the height of the canvas: "))
-canvas_width = int(input("Give the witdth of the canvas: "))
-canv = Canvas(canvas_height,canvas_width)
-chosen_color = str(input("Choose color of the canvas (black/white): "))
-matrix = canv.create_canvas(chosen_color)
+    def create_figure(self, background):
+        self.square = background.canvas[self.x:self.edge, self.y:self.edge] = [255,0,0]
+        
+        return self.square
+
+        
+
+    def select_square_color(self):
+        pass
 
 
+class Rectangle:
+    def __init__(self) -> None:
+        pass
 
 
-
+canv = Canvas(10,10, "white")
+canv_table = canv.create_canvas()
+color = canv.select_color()
+squaree = Square(1,1,"any",3)
+sq = squaree.create_figure(color)
+print(sq)
